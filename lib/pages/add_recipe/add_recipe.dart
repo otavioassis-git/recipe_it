489e66ee2390112ad5c761aaf9d5db8fa8cb5644
otavioassis-git <otavioassis2013@gmail.com>
2025-10-21 19:47:56 -0300

    feat: adding confirmation to description field

--- lib/pages/add_recipe/add_recipe.dart
+++ lib/pages/add_recipe/add_recipe.dart
@@ -203,25 +203,64 @@ class NameSection extends StatelessWidget {
   }
 }
 
-class DescriptionSection extends StatelessWidget {
+class DescriptionSection extends StatefulWidget {
   const DescriptionSection({super.key, required this.descriptionController});
 
   final TextEditingController descriptionController;
 
   @override
+  State<DescriptionSection> createState() => _DescriptionSectionState();
+}
+
+class _DescriptionSectionState extends State<DescriptionSection> {
+  late FocusNode focusNode;
+  bool isFocused = false;
+
+  @override
+  void initState() {
+    focusNode = FocusNode();
+    focusNode.addListener(() => _onFocusChange());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    focusNode.removeListener(() => _onFocusChange());
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  void _onFocusChange() {
+    setState(() {
+      isFocused = focusNode.hasFocus;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      spacing: 8,
       children: [
-        Text('Description'),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Description'),
+            isFocused
+                ? IconButton(
+                    onPressed: () => focusNode.unfocus(),
+                    icon: Icon(Icons.check),
+                  )
+                : const SizedBox(height: 48),
+          ],
+        ),
         Card(
           margin: const EdgeInsets.all(0),
           child: Padding(
             padding: const EdgeInsets.all(12.0),
             child: TextFormField(
-              controller: descriptionController,
+              controller: widget.descriptionController,
               maxLines: null,
+              focusNode: focusNode,
               decoration: InputDecoration(
                 hintText: 'Tell a little bit about the recipe...',
               ),
@@ -393,7 +432,6 @@ class _StepsSectionState extends State<StepsSection> {
     setState(() {
       isFocused[index] = focusNodes[index].hasFocus;
       isKeyboardUp = isFocused.any((e) => e);
-      print('changing focus on $index: $isKeyboardUp');
     });
   }
 
