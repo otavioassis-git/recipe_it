3acaf4e099915441420e1eedd2ecd656d8a472a4
otavioassis-git <otavioassis2013@gmail.com>
2025-11-18 11:52:47 -0300

    refact: moving custom_select_button widget out of appearance_setting

--- lib/pages/settings/widgets/appearance_setting.dart
+++ lib/pages/settings/widgets/appearance_setting.dart
@@ -1,7 +1,7 @@
 import 'package:recipe_it/data/notifiers.dart';
 import 'package:flutter/material.dart';
 import 'package:recipe_it/l10n/app_localizations.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:recipe_it/pages/settings/widgets/custom_select_button.dart';
 
 class AppearanceSetting extends StatelessWidget {
   const AppearanceSetting({super.key});
@@ -63,55 +63,6 @@ class AppearanceSetting extends StatelessWidget {
           ],
         );
       },
-    );
-  }
-}
-
-class CustomSelectButton extends StatelessWidget {
-  const CustomSelectButton({
-    super.key,
-    required this.selectedThemeMode,
-    required this.label,
-    required this.value,
-  });
-
-  final ThemeMode selectedThemeMode;
-  final String label;
-  final ThemeMode value;
-
-  @override
-  Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-
-    final themeModeStringMap = {
-      ThemeMode.system: "ThemeMode.system",
-      ThemeMode.dark: "ThemeMode.dark",
-      ThemeMode.light: "ThemeMode.light",
-    };
-
-    return Expanded(
-      child: TextButton(
-        onPressed: () async {
-          themeModeNotifier.value = value;
-          final SharedPreferences prefs = await SharedPreferences.getInstance();
-          prefs.setString('themeMode', themeModeStringMap[value]!);
-        },
-        style: TextButton.styleFrom(
-          backgroundColor: selectedThemeMode == value
-              ? colorScheme.inversePrimary
-              : colorScheme.surface,
-          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
-        ),
-        child: Text(
-          label,
-          style: TextStyle(
-            color: selectedThemeMode == value
-                ? Colors.white
-                : colorScheme.onSurface,
-          ),
-        ),
-      ),
     );
   }
 }
